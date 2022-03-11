0dc7a25db078744efa66d042d41f0a5cd28f87d4
Raphaël de Fondeville <rdefondeville@gmail.com>
2022-03-11 14:34:03 +0000

    Update install.R

--- install.R
+++ install.R
@@ -2,3 +2,30 @@ install.packages("evd")
 install.packages("evir")
 install.packages("ismev")
 install.packages("SpatialExtremes")
+
+# Update markdown pacakge
+install.packages("markdown")
+
+# Packages for plotting spatial objects
+install.packages("RgoogleMaps")
+install.packages("png")
+install.packages("rjson")
+install.packages("jpeg")
+install.packages("bitops")
+install.packages("plogr")
+
+install.packages("sp")
+install.packages("rgdal")
+install.packages("ggplot2")
+install.packages("evd")
+install.packages("gmp")
+install.packages("mvPot")
+
+install.packages("git2r")
+
+install.packages("usethis")
+install.packages("devtools")
+#devtools::install_github("hadley/devtools")
+devtools::install_github("dkahle/ggmap")
+
+
